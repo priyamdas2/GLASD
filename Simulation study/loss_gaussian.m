ce1f5a377d33b7e97fd96474fa4b0c79645004d3
Priyam Das <41349084+priyamdas2@users.noreply.github.com>
2025-12-24 11:38:54 -0500

    Delete Simulation study directory

--- Simulation study/loss_gaussian.m
+++
@@ -1,5 +0,0 @@
-function val = loss_gaussian(X, C)
-    C_inv = inv(C);
-    d = mahal_squared_distances(X, C_inv);
-    val = 0.5 * size(X,1) * log(det(C)) + 0.5 * sum(d);
-end
--- Simulation study/mahal_squared_distances.m
+++
@@ -1,4 +0,0 @@
-function d = mahal_squared_distances(X, C_inv)  % actually it's squared mahal distance
-% Computes SQUARED Mahalanobis distances for each row in X given C_inv
-    d = sum((X * C_inv) .* X, 2);  % (X C^{-1}) .* X row-wise sum
-end
